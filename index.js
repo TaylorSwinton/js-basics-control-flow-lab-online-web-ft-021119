392fbc81a47143bd19fe00b288ebf8cc88b72a21
TaylorSwinton <taylorswinton1@gmail.com>
2019-05-28 11:48:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let result
+  if (ride <= 400) {
+    result = "This one is on me!"
+  } else if (ride > 2000 && ride <= 2500) {
+    result = "I will gladly take your thirty bucks."
+  } else {
+    result = "No can do." 
+  }
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return (city === "NYC"  ? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let response
+  switch(tip){
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+  }
+  return response
 }
